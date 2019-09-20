7de565bddf79674ee785e15ecbf5bcbcb1c7bdc1
Ajay Banstola <ajay.banstola@gmail.com>
2019-09-20 17:19:55 +0545

    upto cart done

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
-        <h1>Hello from product</h1>
-      </div>
+      <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div class="card text-left">
+          <img class="card-img-top" src="holder.js/100px180/" alt="" />
+          <div class="card-body">
+            <h4 class="card-title">{title}</h4>
+            <p class="card-text">{img}</p>
+          </div>
+        </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
