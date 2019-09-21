85127079e1b4a08387e9ec15134768feb11f03b6
Ajay Banstola <ajay.banstola@gmail.com>
2019-09-21 11:25:22 +0545

    cheatsheet for card done

--- src/components/Product.js
+++ src/components/Product.js
@@ -8,7 +8,7 @@ export default class Product extends Component {
     const { id, title, img, price, inCart } = this.props.product;
     return (
       <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3">
-        <div claassName="card">
+        <div className="card">
           <div
             className="img-container p-5"
             onClick={() => {
@@ -34,6 +34,14 @@ export default class Product extends Component {
                 <i className="fas fa-cart-plus" />
               )}
             </button>
+          </div>
+          {/*card footer */}
+          <div className="card-footer d-flex justify-content-between">
+            <p className="align-self-center mb-0">{title}</p>
+            <h5 className="text-blue font-italic mb-0">
+              <span className="mr-1">$</span>
+              {price}
+            </h5>
           </div>
         </div>
       </ProductWrapper>
