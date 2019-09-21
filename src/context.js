f0d9c450a5f3bc33593820623efb2891817cf814
Ajay Banstola <ajay.banstola@gmail.com>
2019-09-21 15:01:23 +0545

    fixed the reference pass error

--- src/context.js
+++ src/context.js
@@ -5,9 +5,23 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
+    products: [],
     detailProduct: detailProduct
   };
+  componentDidMount() {
+    this.setProducts();
+  }
+  setProducts = () => {
+    let tempProducts = [];
+    storeProducts.forEach(item => {
+      const singleItem = { ...item };
+      tempProducts = [...tempProducts, singleItem];
+    });
+    this.setState(() => {
+      return { products: tempProducts };
+    });
+  };
+
   handleDetail = () => {
     console.log("hello from detail");
   };
