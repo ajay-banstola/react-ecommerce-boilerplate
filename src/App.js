5ae79f7f426799ca3443d07021dc0d7f7fe92248
Ajay Banstola <ajay.banstola@gmail.com>
2019-09-23 07:47:10 +0545

    added the login form

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import ProductList from "./components/ProductList";
 import Details from "./components/Details";
 import Default from "./components/Default";
 import Cart from "./components/Cart";
+import ValidatedLoginForm from "./components/LoginForm";
 import { Switch, Route } from "react-router-dom";
+import "./css/style.css";
 
 class App extends Component {
   render() {
@@ -18,6 +20,7 @@ class App extends Component {
           <Route exact path="/" component={ProductList} />
           <Route path="/details" component={Details} />
           <Route path="/cart" component={Cart} />
+          <Route path="/login" component={ValidatedLoginForm} />
           <Route component={Default} />
         </Switch>
       </Fragment>
